6db742c07810b12df3c0ab73f95f0c697fd32f49
CD202506 <ncc481@gmail.com>
2025-12-17 23:39:47 +0800

    fix(v1): resolve knowledge page wrapper typing

--- src/app/knowledge/page.tsx
+++ src/app/knowledge/page.tsx
@@ -2,8 +2,6 @@ import ClinicalMapPage from "../clinical/map/page";
 
 export const dynamic = "force-dynamic";
 
-export default async function KnowledgePage(
-  props: Parameters<typeof ClinicalMapPage>[0],
-) {
-  return ClinicalMapPage(props);
+export default function KnowledgePage() {
+  return ClinicalMapPage();
 }
